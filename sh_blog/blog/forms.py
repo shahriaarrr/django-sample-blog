b50b7208667f0dde2db395f92db0213e0ecf81c6
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-07-24 12:40:30 +0430

    check fullname in form

--- sh_blog/blog/forms.py
+++ sh_blog/blog/forms.py
@@ -6,3 +6,10 @@ class SignUpForm(forms.Form):
     email = forms.EmailField()
     fullname = forms.CharField()
     phonenumber = forms.IntegerField(required=False)
+
+    def clean_fullname(self):
+        name = self.cleaned_data['fullname']
+        if name.istitle() == False:
+            message = "Error, The first letter of your first and last name must be uppercase."
+            raise forms.ValidationError(message)
+        return name
