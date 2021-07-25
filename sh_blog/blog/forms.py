15029a9c6a207ea2e0f092328e7fa3b1474774ce
shahriaarrr <shahriaarrr@users.noreply.github.com>
2021-07-25 21:55:12 +0430

    make SignUpForm better

--- sh_blog/blog/forms.py
+++ sh_blog/blog/forms.py
@@ -1,15 +1,18 @@
 from django import forms
+from .models import User
 
-class SignUpForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField()
-    email = forms.EmailField()
-    fullname = forms.CharField()
-    phonenumber = forms.IntegerField(required=False)
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
+        
+    
 
+'''
     def clean_fullname(self):
         name = self.cleaned_data['fullname']
         if name.istitle() == False:
             message = "Error, The first letter of your first and last name must be uppercase."
             raise forms.ValidationError(message)
         return name
+'''
